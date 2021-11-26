6fe94d63a1e7377b08706be1da53b82996b71099
bhvoetberg <b.h.voetberg@gmail.com>
2021-11-26 14:42:44 +0100

    basic structure for remote, CIM and Wallbracket

---
+++ src/main/java/com/example/demo/models/Remote.java
@@ -0,0 +1,68 @@
+package com.example.demo.models;
+
+public class Remote {
+    private long id;
+    private String compatibleWidth;
+    private String batteryType;
+    private String name;
+    private String brand;
+    private Double price;
+    private Integer originalStock;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCompatibleWidth() {
+        return compatibleWidth;
+    }
+
+    public void setCompatibleWidth(String compatibleWidth) {
+        this.compatibleWidth = compatibleWidth;
+    }
+
+    public String getBatteryType() {
+        return batteryType;
+    }
+
+    public void setBatteryType(String batteryType) {
+        this.batteryType = batteryType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getOriginalStock() {
+        return originalStock;
+    }
+
+    public void setOriginalStock(Integer originalStock) {
+        this.originalStock = originalStock;
+    }
+
+}
